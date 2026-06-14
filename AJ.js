d88a47bd672cf10cf3dc6c175e65f95b99696f5d
AJC2007 <ahmadjanch@gmail.com>
2026-06-14 17:21:22 +0500

    Add files via upload

---
+++ AJ.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function aj(){
+	var a = document.getElementById("a").value
+	var b = document.getElementById("b").value
+	var cn = a + b
+	document.getElementById("d").innerHTML=cn
+	
+}
